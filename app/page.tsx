fd22e6d9dde2c3c43ea3032c1cf6c710435e7a8c
Vince Picone <vp@vincepic.one>
2025-05-01 14:08:05 -0400

    goals

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,20 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {isSignedIn && (
-          <Link
-            href="/journal"
-            className="rounded bg-blue-600 text-white px-4 py-2 mt-2 inline-block hover:bg-blue-700 transition-colors"
-          >
-            Go to Journal
-          </Link>
+          <>
+            <Link
+              href="/journal"
+              className="rounded bg-blue-600 text-white px-4 py-2 mt-2 inline-block hover:bg-blue-700 transition-colors"
+            >
+              Go to Journal
+            </Link>
+            <Link
+              href="/goals"
+              className="rounded bg-blue-600 text-white px-4 py-2 mt-2 inline-block hover:bg-blue-700 transition-colors"
+            >
+              Go to Goals
+            </Link>
+          </>
         )}
       </main>
     </div>
